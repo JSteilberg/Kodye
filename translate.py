f0b56670b60773eac2c89ea069e8c195f9709fca
Jackson Steilberg <steilberg.j@ccs.neu.edu>
2017-09-30 14:30:28 -0400

    Added ability to translate brainfuck to midi

--- translate.py
+++ translate.py
@@ -1,5 +1,5 @@
 from test import *
-from mido import MidiFile
+from mido import Message, MidiFile, MidiTrack
 
 
 def to_brainfrick(note_num):
@@ -25,3 +25,33 @@ for i, track in enumerate(mid.tracks):
         brainfrick += to_brainfrick((msg.bytes()[1] % 12) + 60)
 
     print(brainfrick)
+
+	
+def brainfrick_to_midi(bf_string):
+    mid = MidiFile()
+    track = MidiTrack()
+    mid.tracks.append(track)
+
+    track.append(Message('program_change', program=12, time=0))
+
+    for bf_char in bf_string:
+        onote = internal_bf_to_int(bf_char)
+        if onote is -1:
+            continue
+        track.append(Message('note_on', note=onote, velocity=70, time=32))
+        track.append(Message('note_off', note=onote, velocity=127, time=32))
+        print(onote)
+    
+    mid.save('new_song.mid')
+    
+def internal_bf_to_int(bf_string):
+    return {
+        "+" : 60,
+        "-" : 61,
+        "<" : 62,
+        ">" : 64,
+        "[" : 65,
+        "]" : 67,
+        "." : 69,
+        "," : 71
+        }.get(bf_string, -1)
