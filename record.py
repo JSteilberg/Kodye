fa5c14601d50224724f4f11ef4ccca0a76e23207
Daniel Cohen <cohen.dan@husky.neu.edu>
2017-09-30 17:51:11 -0400

    overwrite files correctly

--- record.py
+++ record.py
@@ -1,5 +1,6 @@
 import pyaudio
 import wave
+import os, sys
 
 RATE = 44100
 CHUNK = 1024
@@ -11,7 +12,7 @@ def mkStream(audio, **args):
 # Int -> [Frame]
 def recordAudio(seconds):
     audio = pyaudio.PyAudio()
-    stream = mkStream(audio)
+    stream = mkStream(audio, stream_callback=callback)
     frames = int(RATE / CHUNK * seconds)
     result = [ stream.read(CHUNK) for _ in range(frames) ] # this is where the magic happens
     stream.stop_stream()
@@ -33,12 +34,18 @@ def playAudio(frames):
     play.terminate()
 
 def writeWav(frames, outName):
+    try:
+        os.remove(outName)
+    except:
+        pass
+
     wavFile = wave.open(outName, 'wb')
     wavFile.setnchannels(1)
     wavFile.setsampwidth(2)
     wavFile.setframerate(RATE)
     wavFile.writeframes(b''.join(frames))
     wavFile.close()
+
 
 if __name__ == '__main__':
     writeWav(recordAudio(seconds = 5))
